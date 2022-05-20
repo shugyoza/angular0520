28a7b2291c76855c1b19fd17c1ba068557244219
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-05-20 08:35:30 -0700

    created settings component and the html wireframe, deleted setting component.

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ my-app/src/app/settings/settings.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.sass']
+})
+export class SettingsComponent implements OnInit {
+
+  constructor() { }
+
+  title = 'Bruh';
+
+  ngOnInit(): void {
+  }
+
+}
