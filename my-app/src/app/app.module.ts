45a4b57536d908c2088aa3209db02c362df7f74a
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-05-20 09:10:18 -0700

    created admin component and the html wireframe.

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SettingsComponent } from './settings/settings.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SettingsComponent,
+    AdminComponent,
   ],
   imports: [
     BrowserModule,
